c81df124bd6e40d2224207a8bc4fd89c8c0b411e
Zoltan Gyebrovszki <gyeby5@gmail.com>
2018-01-06 10:28:47 +0100

    missing file

---
+++ GetApi/Arguments.cs
@@ -0,0 +1,48 @@
+﻿using SenseNet.Tools.CommandLineArguments;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceBender.ApiExplorer.GetApi
+{
+    internal class Arguments
+    {
+        [NoNameOption(order: 0, required: true, nameInHelp: "source", helpText: "Path of the directory containing assemblies.")]
+        private string sourceDirectoryArg { get; set; }
+        private string _sourceDirectory;
+        public string SourceDirectory
+        {
+            get
+            {
+                if (_sourceDirectory == null)
+                    _sourceDirectory = Path.GetFullPath(sourceDirectoryArg);
+                return _sourceDirectory;
+            }
+        }
+
+        [NoNameOption(order: 1, required: false, nameInHelp: "target", helpText: "Path of output file. Default: <source\\api.txt>")]
+        private string targetFileArg { get; set; }
+        private string _targetFile;
+        public string TargetFile
+        {
+            get
+            {
+                if (_targetFile == null)
+                {
+                    _targetFile = targetFileArg != null
+                        ? Path.GetFullPath(targetFileArg)
+                        : Path.Combine(SourceDirectory, "api.txt");
+                }
+                return _targetFile;
+            }
+        }
+
+        [CommandLineArgument(name: "withinternals", required: false, aliases: "i+", helpText: "Shows internal classes and members")]
+        public bool WithInternals { get; set; }
+        [CommandLineArgument(name: "onlyinternals", required: false, aliases: "i-", helpText: "Shows only internal classes and members")]
+        public bool OnlyInternals { get; set; }
+    }
+}
